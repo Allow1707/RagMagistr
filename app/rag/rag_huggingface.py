862812b2a9f6d1e6ec11afadb90d04244419a028
Алексей <alex@stargaze.ru>
2025-05-23 11:21:21 +0300

    2025-05-23. refactor: новая логика возрата подходящих текство с трешхолдом

--- app/rag/rag_huggingface.py
+++ app/rag/rag_huggingface.py
@@ -1,7 +1,7 @@
 import os
 from langchain_chroma import Chroma
 from langchain.docstore.document import Document
-from RagMagistr.app.utils import get_embeddings_model, timing_decorator
+from app.utils import get_embeddings_model, timing_decorator
 
 # Получаем абсолютный путь к директории текущего файла
 CURRENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -10,11 +10,16 @@ CHROMA_CACHE_PATH = os.path.join(CURRENT_DIR, "chroma_db")
 
 
 @timing_decorator
-def rag_huggingface(question: str) -> list[Document]:
+def rag_huggingface(question: str) -> list:
     embedding = get_embeddings_model()
     vectorstore = Chroma(
         persist_directory=CHROMA_CACHE_PATH,
         embedding_function=embedding
     )
-    docs: list[Document] = vectorstore.similarity_search(question)
-    return docs
+    docs = vectorstore.similarity_search_with_relevance_scores(question, k=6)
+    content = []
+    for text, score in docs:
+        print(score)
+        if score >= 0.77:
+            content.append(text)
+    return content
